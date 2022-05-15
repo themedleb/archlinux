54deb5cfb9a214e86a034281b9343973753721f3
themedleb <30783835+themedleb@users.noreply.github.com>
2022-05-15 00:10:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ read -p "Hostname: " hostname
 read -p "Root password: " rootPassword
 read -p "Username: " userName
 read -p "User password: " userPassword
-read -p "Graphics driver ('amd' or 'intel' or 'nvidia' or 'allopensource' default is 'vm'): " graphicsDriver
+read -p "Graphics driver ('amd' or 'intel' or 'nvidia' or 'allopensource'. Default is 'vm'): " graphicsDriver
 
 if [[ $graphicsDriver == 'amd' ]]
 then
@@ -30,7 +30,11 @@ fi
 
 echo '{"!encryption-password":"'$storageEncryptionPassword'","!root-password":"'$rootPassword'", "!superusers":{},"!users":{"'$userName'": {"!password": "'$userPassword'"}}}' | jq '.' > user_credentials.json
 
-echo '{"/dev/'$storageDevicePath'": {"partitions": [{"boot": true,"encrypted": false,"filesystem": {"format": "fat32"},"mountpoint": "/boot","size": "512MiB","start": "1MiB","type": "primary","wipe": true},{"btrfs": {"subvolumes": {"@": "/","@snapshots": "/.snapshots","@home": "/home","@log": "/var/log","@pkg": "/var/cache/pacman/pkg"}},"encrypted": true,"filesystem": {"format": "btrfs","mount_options": ["compress=zstd"]},"mountpoint": null,"size": "100%","start": "513MiB","type": "primary","wipe": true}],"wipe": true}}' | jq '.' > user_disk_layout.json
+echo '{"/dev/'$storageDevicePath'": {"partitions": [{"boot": true,"encrypted": false,"filesystem": {"format": "fat32"},"mountpoint": "/boot","size": "512MiB","start": "1MiB","type": "primary","wipe": true},{"btrfs": {"subvolumes": {"@": "/","@snapshots": "/.snapshots","@home": "/home","@log": "/var/log","@pkg": "/var/cache/pacman/pkg"}},"encrypted": true,"filesystem": {"format": "btrfs","mount_options": ["compress=zstd"]},"mountpoint": null,"size": "100%","start": "513MiB","type": "primary","wipe": true}],"wipe": true}}' | jq '.' > user_disk_layout.json &
+
+pid=$!
+wait $pid
+echo "Process with PID $pid has finished with Exit status: $?"
 
 tmp=$(mktemp)
 
